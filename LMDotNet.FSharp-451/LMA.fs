220964fdcab525b9c37daccb8a691d2993841bde
Frank Niemeyer <frank.niemeyer@gmail.de>
2014-02-11 14:15:16 +0100

    Started work on an F# interface

---
+++ LMDotNet.FSharp-451/LMA.fs
@@ -0,0 +1,78 @@
+﻿namespace LMDotNet.FSharp
+
+type SolverSettings = 
+    { /// <summary>Relative error desired in the sum of squares.
+      /// Termination occurs when both the actual and
+      /// predicted relative reductions in the sum of squares
+      /// are at most ftol.</summary>
+      ftol: float
+
+      /// <summary> Relative error between last two approximations.
+      /// Termination occurs when the relative error between
+      /// two consecutive iterates is at most xtol.</summary>
+      xtol: float
+
+      /// <summary> Orthogonality desired between fvec and its derivs.
+      /// Termination occurs when the cosine of the angle
+      /// between fvec and any column of the Jacobian is at
+      /// most gtol in absolute value. (measure of degeneracy) </summary>
+      gtol: float
+
+      /// <summary> Step used to calculate the Jacobian, should be
+      /// slightly larger than the relative error in the
+      /// user - supplied functions.</summary>
+      epsilon: float
+
+      /// <summary> Used in determining the initial step bound. This
+      /// bound is set to the product of stepbound and the
+      /// Euclidean norm of diag*x if nonzero, or else to
+      /// stepbound itself. In most cases stepbound should lie
+      /// in the interval (0.1,100.0). Generally, the value
+      /// 100.0 is recommended.</summary>         
+      initialStepbound: float
+
+      /// <summary>
+      /// Used to set the maximum number of function evaluations
+      /// to patience*(number_of_parameters+1)
+      /// </summary>
+      maxIterations: int
+
+      /// <summary>
+      /// If true, the variables will be rescaled internally. Recommended value is 1.
+      /// </summary>
+      scaleDiagonal: bool
+
+      /// <summary>
+      /// true: print status messages to stdout
+      /// </summary>
+      verboseOutput: bool }
+
+module LMA =
+    open System
+    open LMDotNet
+    
+    let defaultSettings =
+        let defaultTolerance = 1.0e-14;
+        { ftol = defaultTolerance
+          xtol = defaultTolerance
+          gtol = defaultTolerance
+          epsilon = defaultTolerance
+          initialStepbound = 100.0
+          maxIterations = 100
+          scaleDiagonal = true
+          verboseOutput = false }
+
+    let init settings =
+        let solver = LMSolver()
+        solver.Ftol <- settings.ftol
+        solver.Xtol <- settings.xtol
+        solver.Gtol <- settings.gtol
+        solver.Epsilon <- settings.epsilon
+        solver.InitialStepbound <- settings.initialStepbound
+        solver.ScaleDiagonal <- settings.scaleDiagonal
+        solver.VerboseOutput <- settings.verboseOutput
+        solver
+
+    let minimize settings x0 n f =
+        let solver = init settings        
+        solver.Minimize(Action<_,_>(f), x0, n)
---
+++ LMDotNet.FSharpTest/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open LMDotNet.FSharp
+
+[<EntryPoint>]
+let main argv = 
+    let findMin = { LMA.defaultSettings with verboseOutput = true }
+                  |> LMA.minimize
+
+    let res = fun (p: float[]) (r: float[]) -> r.[0] <- p.[1] - p.[0]
+                                               r.[1] <- p.[1] - 2.0 * p.[0] + 0.5
+              |> findMin [|0.0; 0.0|] 2
+
+    printfn "%s" res.Message
+    printfn "%g %g" res.OptimizedParameters.[0] res.OptimizedParameters.[1]
+    
+    0 // return an integer exit code
