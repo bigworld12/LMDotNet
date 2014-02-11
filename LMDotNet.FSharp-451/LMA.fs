0b6476195344a1baad924c94c2acf0e5adc4866b
Frank Niemeyer <frank.niemeyer@gmail.de>
2014-02-11 14:55:15 +0100

    F# API covers all of the LMSolver interface

--- LMDotNet.FSharp-451/LMA.fs
+++ LMDotNet.FSharp-451/LMA.fs
@@ -73,6 +73,26 @@ module LMA =
         solver.VerboseOutput <- settings.verboseOutput
         solver
 
-    let minimize settings x0 n f =
-        let solver = init settings        
-        solver.Minimize(Action<_,_>(f), x0, n)
+    /// <summary>
+    /// Determines the vector x that minimizes the squared L2-norm of a user-supplied
+    /// function f, i.e. it determines x_opt = argmin_x ||f(x)||²
+    /// </summary>
+    /// <param name="solver">Configured solver<br/>
+    /// <param name="f">Evaluates the system at the point x;<br/>
+    /// first parameter:  x    (IN;  length = length(initialGuess));
+    /// second parameter: f(x) (OUT; length = nDataPoints)</param>
+    /// <param name="x0">Initial guess for x_opt; length determines the length of x</param>
+    /// <param name="n">Length of f(x) 
+    /// == number of datapoints (for regression)        
+    /// == number of equations (for solving NLS)
+    /// == length of the residue vector; invariant: n &gt;= length(x0)</param>
+    /// <returns>Optimum x_opt (if successful) and solution status</returns>
+    let inline minimize (solver: LMSolver) x0 n f = solver.Minimize(Action<_,_>(f), x0, n)
+
+    let inline solve (solver: LMSolver) x0 f = solver.Solve(Action<_,_>(f), x0)
+
+    let inline fitCurve (solver: LMSolver) beta0 xs ys f = solver.FitCurve(Func<_,_,_>(f), beta0, xs, ys)
+
+    let inline fitSurface (solver: LMSolver) beta0 xs ys zs f = solver.FitSurface(Func<_,_,_,_>(f), beta0, xs, ys, zs)
+
+    let inline fit (solver: LMSolver) beta0 grid data f = solver.Fit(Func<_,_,_>(f), beta0, grid, data)
--- LMDotNet.FSharpTest/Program.fs
+++ LMDotNet.FSharpTest/Program.fs
@@ -3,14 +3,21 @@ open LMDotNet.FSharp
 
 [<EntryPoint>]
 let main argv = 
-    let findMin = { LMA.defaultSettings with verboseOutput = true }
-                  |> LMA.minimize
+    // set up solver
+    let solverEngine = { LMA.defaultSettings with verboseOutput = true } 
+                       |> LMA.init
 
-    let res = fun (p: float[]) (r: float[]) -> r.[0] <- p.[1] - p.[0]
-                                               r.[1] <- p.[1] - 2.0 * p.[0] + 0.5
-              |> findMin [|0.0; 0.0|] 2
+    let findMin = LMA.minimize solverEngine
+    
+    let res1 = fun (p: float[]) (r: float[]) -> r.[0] <- p.[1] - p.[0]
+                                                r.[1] <- p.[1] - 2.0 * p.[0] + 0.5
+               |> findMin [|0.0; 0.0|] 2
 
-    printfn "%s" res.Message
-    printfn "%g %g" res.OptimizedParameters.[0] res.OptimizedParameters.[1]
+    let res2 = fun (p: float[]) (r: float[]) -> r.[0] <- p.[1] - p.[0]
+                                                r.[1] <- p.[1] - 2.0 * p.[0] + 0.5
+               |> findMin [|1.0; 1.0|] 2
+
+    printfn "%s" res1.Message
+    printfn "%g %g" res1.OptimizedParameters.[0] res1.OptimizedParameters.[1]
     
     0 // return an integer exit code
