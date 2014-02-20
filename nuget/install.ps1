f36a47506bb54256778f57924230c29a01d3a9b4
Frank Niemeyer <frank.niemeyer@gmail.de>
2014-02-20 15:35:50 +0100

    Updated nuspecs

--- nuget/install.ps1
+++ nuget/install.ps1
@@ -1,9 +1,21 @@
 param($installPath, $toolsPath, $package, $project)
 
+# set lmfit32 build options
 $lmfit = $project.ProjectItems.Item("lmfit32").ProjectItems.Item("lmfit.dll")
-$lmfit.Properties.Item("BuildAction").Value = 0 # BuildAction = None
+if ($project.Type -eq "F#") {
+    $lmfit.Properties.Item("BuildAction").Value = [Microsoft.VisualStudio.FSharp.ProjectSystem.BuildAction]::None
+}
+else {
+    $lmfit.Properties.Item("BuildAction").Value = 0 # None
+}
 $lmfit.Properties.Item("CopyToOutputDirectory").Value = 1 # CopyToOutputDirectory = Copy always
 
+# set lmfit64 build options
 $lmfit = $project.ProjectItems.Item("lmfit64").ProjectItems.Item("lmfit.dll")
-$lmfit.Properties.Item("BuildAction").Value = 0 # BuildAction = None
+if ($project.Type -eq "F#") {
+    $lmfit.Properties.Item("BuildAction").Value = [Microsoft.VisualStudio.FSharp.ProjectSystem.BuildAction]::None
+}
+else {
+    $lmfit.Properties.Item("BuildAction").Value = 0 # None
+}
 $lmfit.Properties.Item("CopyToOutputDirectory").Value = 1 # CopyToOutputDirectory = Copy always
