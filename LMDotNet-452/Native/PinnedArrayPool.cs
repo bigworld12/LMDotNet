88761ea168544a1e18e1fbb2f9fbbf2a2b1a600b
Frank Niemeyer <frank.niemeyer@gmail.com>
2015-06-02 18:38:11 +0200

    [PinnedArray]: moved to separate file

--- LMDotNet-452/Native/PinnedArrayPool.cs
+++ LMDotNet-452/Native/PinnedArrayPool.cs
@@ -1,35 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 namespace LMDotNet.Native
 {
-    /// <summary>
-    /// An array pinned to the heap
-    /// </summary>
-    /// <typeparam name="T">Type of the elements of the array</typeparam>
-    struct PinnedArray<T> where T: struct //:IDisposable
-    {
-        public readonly T[] Array;
-        public readonly GCHandle Handle;
-        public readonly IntPtr Address;
-
-        public PinnedArray(T[] array) {
-            this.Array = array;
-            this.Handle = GCHandle.Alloc(array, GCHandleType.Pinned);
-            this.Address = Handle.AddrOfPinnedObject();
-        }
-
-        public void Unpin() {
-            Handle.Free();            
-        }
-
-        public static implicit operator T[](PinnedArray<T> pinnedArray) {
-            return pinnedArray.Array;
-        }
-    }
-
     // from http://blog.getpaint.net/2012/04/30/marshaling-native-arrays-back-as-managed-arrays-without-copying/
 
     /// <summary>
